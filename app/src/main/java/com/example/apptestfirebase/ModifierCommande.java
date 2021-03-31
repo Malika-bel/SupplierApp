02b6abb8c5c32eee40ef194d9c882b5468a9250d
Adlane Gasmi <gasmi.adlane@gmail.com>
2021-03-31 17:05:23 +0200

    Adlane Commit| Ajouter, supprimer et modiffier

--- app/src/main/java/com/example/apptestfirebase/ModifierCommande.java
+++ app/src/main/java/com/example/apptestfirebase/ModifierCommande.java
@@ -5,18 +5,23 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class ModifierCommande extends AppCompatActivity {
     Button ModifierCommandeFinal ;
     EditText Commande,TypeProduit, DateLivraison, Client, Consigne;
+    FirebaseDatabase database ;
+    DatabaseReference ref_commande_update;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_modifier_commande);
-      /*  Intent intent = getIntent();
-        String Commande = intent.getStringExtra("commande"); */
+
         ModifierCommandeFinal = findViewById(R.id.ModifierCommandeFinal);
 
         Commande = findViewById(R.id.Commande);
@@ -25,17 +30,28 @@ public class ModifierCommande extends AppCompatActivity {
         Consigne= findViewById(R.id.Consigne);
         Client= findViewById(R.id.Client);
 
+        ref_commande_update = FirebaseDatabase.getInstance().getReference("Commande").child(" ");
+
         ModifierCommandeFinal.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(ModifierCommande.this, ConfirmationAjoutCommande.class);
                 startActivity(intent);
 
-                final String commande = Commande.getText().toString().trim();
-                final String typeProduit = TypeProduit.getText().toString().trim();
-                final String dateLivraison  = DateLivraison .getText().toString().trim();
-                final String client = Client.getText().toString().trim();
-                final String consigne  = Consigne .getText().toString().trim();
+                 String commande = Commande.getText().toString().trim();
+                 String typeProduit = TypeProduit.getText().toString().trim();
+                 String dateLivraison  = DateLivraison .getText().toString().trim();
+                 String client = Client.getText().toString().trim();
+                 String consigne  = Consigne .getText().toString().trim();
+
+                 ref_commande_update.child("Commande").setValue(commande) ;
+                ref_commande_update.child("TypeProduit").setValue(typeProduit) ;
+                ref_commande_update.child("DateLivraison").setValue(dateLivraison) ;
+                ref_commande_update.child("Client").setValue(client) ;
+                ref_commande_update.child("Consigne").setValue(consigne) ;
+
+                Toast.makeText(ModifierCommande.this, "Votre commande a été mise à jours", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(getApplicationContext(),MainActivity.class));
 
             }
         });
--- app/src/main/java/com/example/apptestfirebase/SupprimerCommande.java
+++ app/src/main/java/com/example/apptestfirebase/SupprimerCommande.java
@@ -4,15 +4,24 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class SupprimerCommande extends AppCompatActivity {
 Button Oui, Non ;
+    FirebaseDatabase database ;
+    DatabaseReference ref_commande_suppr;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_supprimer_commande);
+        database= FirebaseDatabase.getInstance();
+
+        ref_commande_suppr=FirebaseDatabase.getInstance().getReference("Commande").child(" ");
 
         Oui = findViewById(R.id.Oui);
         Non = findViewById(R.id.Non);
@@ -20,7 +29,12 @@ Button Oui, Non ;
         Oui.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // requete pour supprimer l'objet
+
+                ref_commande_suppr.removeValue() ;
+
+                Toast.makeText(SupprimerCommande.this, "Votre commande a été supprimée", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+
             }
         });
 
@@ -29,6 +43,7 @@ Button Oui, Non ;
             public void onClick(View v) {
                 Intent intent = new Intent(SupprimerCommande.this, AjouterCommande.class);
                 startActivity(intent);
+
             }
         });
 
